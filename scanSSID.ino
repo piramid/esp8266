48997984725ed610ff1cce04bd0c1aec21704a18
piramid <pornchai.tom@gmail.com>
2023-11-01 09:14:51 +0700

    Add files via upload

---
+++ scanSSID.ino
@@ -0,0 +1,52 @@
+/*
+    ESP8266 WiFi Network Scanner Example
+    Hardware: NodeMCU
+    Date: 1/11/2023
+    Power by Mr. Pornchai Thong-in
+*/
+#include <ESP8266WiFi.h>
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println(""); //Remove garbage
+
+  // Set WiFi to station mode and disconnect from an AP if it was previously connected
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();  //ESP has tendency to store old SSID and PASSword and tries to connect
+  delay(100);
+
+  Serial.println("WiFi Network Scan Started");
+}
+
+void loop() {
+ 
+  // WiFi.scanNetworks will return the number of networks found
+  int n = WiFi.scanNetworks();
+   
+  Serial.println("Scan done");
+  
+  if (n == 0)
+    Serial.println("No Networks Found");
+  else
+  {
+    Serial.print(n);
+    Serial.println(" Networks found");
+    for (int i = 0; i < n; ++i)
+    {
+      // Print SSID and RSSI for each network found
+      Serial.print(i + 1);  //Sr. No
+      Serial.print(": ");
+      Serial.print(WiFi.SSID(i)); //SSID
+      Serial.print(" (");
+      Serial.print(WiFi.RSSI(i)); //Signal Strength
+      Serial.print(") MAC:");
+      Serial.print(WiFi.BSSIDstr(i));
+      Serial.println((WiFi.encryptionType(i) == ENC_TYPE_NONE)?" Unsecured":" Secured");
+      delay(10);
+    }
+  }
+  Serial.println("");
+
+  // Wait a bit before starting New scanning again
+  delay(5000);
+}
